faf2349364c34e172fe605d159981b9a7be8e13b
Arsen2003 <arsen_nurdavletov@mail.ru>
2023-04-27 15:45:52 +0600

    debug json

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,32 +1,18 @@
 import React, { Component } from 'react'
 import TableStores from '../components/TableStores'
-import { API } from '../consts'
+import data from '../db.json'
 export default class Main extends Component {
   state = {
     isLoading: true,
     stores: [],
     error: null,
   }
-  getFetchUsers() {
-    this.setState(
-      {
-        isLoading: true,
-      },
-      () => {
-        fetch(API)
-          .then((res) => res.json())
-          .then((result) =>
-            this.setState({
-              isLoading: false,
-              stores: result,
-            })
-          )
-          .catch(console.log)
-      }
-    )
-  }
   componentDidMount() {
-    this.getFetchUsers()
+    this.setState({
+      isLoading: false,
+      stores: data.stores,
+    })
+ 
   }
   render() {
     return <TableStores {...this.state} />
